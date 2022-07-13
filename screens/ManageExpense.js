7b8b968cb7564256b93403f5fee72494f0ee2389
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-07-13 15:48:00 -0500

    created reusable button comps addl styling

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,8 @@
-import { Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { useLayoutEffect } from 'react';
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../constants/styles';
+import Button from '../components/UI/Buttons';
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -12,9 +15,56 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  return <Text>AllExpenses screen</Text>;
+  function deleteExpenseHandler() {}
+
+  function cancelHandler() {}
+
+  function confirmHandler() {}
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? 'Update' : 'Add'}
+        </Button>
+      </View>
+      <View style={styles.deleteContainer}>
+        <IconButton
+          icon="trash"
+          size={36}
+          color={GlobalStyles.colors.error500}
+          onPress={deleteExpenseHandler}
+        />
+      </View>
+    </View>
+  );
 }
 
 export default ManageExpense;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});

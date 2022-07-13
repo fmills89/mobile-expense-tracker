6ae5ecef2502d8f8c7d93bd1d3c2f9b49e6c4235
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-07-13 10:38:26 -0500

    addl styling to expenseitem screen

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,7 +1,8 @@
 import { Text, FlatList, StyleSheet } from 'react-native';
+import ExpenseItem from './ExpenseItem';
 
 function renderExpenseItem(itemData) {
-  return <Text>{itemData.item.description}</Text>;
+  return <ExpenseItem {...itemData.item} />;
 }
 
 function ExpensesList({ expenses }) {
--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -8,25 +8,25 @@ const DUMMY_EXPENSES = [
     id: 'e1',
     description: 'A pair of shoes',
     amount: 59.99,
-    data: new Date('2021-12-19'),
+    date: new Date('2021-12-19'),
   },
   {
     id: 'e2',
     description: 'Shorts',
     amount: 19.6,
-    data: new Date('2021-12-21'),
+    date: new Date('2021-12-21'),
   },
   {
     id: 'e3',
     description: 'Jeans',
     amount: 69.3,
-    data: new Date('2021-12-22'),
+    date: new Date('2021-12-22'),
   },
   {
     id: 'e4',
     description: 'Groceries',
     amount: 120.13,
-    data: new Date('2021-12-31'),
+    date: new Date('2021-12-31'),
   },
 ];
 

6f066c17b4db28fae9441355930b5fd8ff59fa07
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-07-28 10:50:43 -0500

    added activty indicator

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,11 +1,13 @@
 import { Text, StyleSheet } from 'react-native';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../utils/date';
 import { fetchExpenses } from '../utils/http';
+import LoadingOverlay from '../components/UI/LoadingOverlay';
 
 function RecentExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
   const expensesCtx = useContext(ExpensesContext);
 
   useEffect(() => {
@@ -13,12 +15,19 @@ function RecentExpenses() {
     // should not do that - discouraged
     // work around instead of creating useEffect into async
     async function getExpenses() {
+      setIsFetching(true);
       const expenses = await fetchExpenses();
+      setIsFetching(false);
       // set expenses we got to useState
       expensesCtx.setExpenses(expenses);
     }
+
     getExpenses();
   }, []);
+
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
 
   const recentExpenses = expensesCtx.expenses.filter(expense => {
     const today = new Date();
